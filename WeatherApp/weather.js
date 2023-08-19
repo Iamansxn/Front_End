40ca9697b14653a2fff8bed2b27057bc55629b83
Lol9x <aman99saxena@gmail.com>
2023-08-19 14:13:35 +0530

    commit2

--- WeatherApp/weather.js
+++ WeatherApp/weather.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   const baseUrl = "http://api.weatherapi.com/v1/";
   const apiKey = "ac513c2ccca6427c912105247231808";
-  $("#search").on("click", function () {
+  $("#cityname").on("keypress", function () {
     const city = $("#cityname").val();
     console.log(city);
     const url = `${baseUrl}current.json?key=${apiKey}&q=${city}`;
@@ -24,10 +24,11 @@ $(document).ready(function () {
     const state = data.location.region;
     const country = data.location.country;
     const temp = data.current.temp_c;
+    const condi = data.current.condition.text;
     const uv = data.current.uv;
-    var text = `<h1>${temp + "°C"}</h1><h2>${name}</h2><h2>${
-      "Region:" + state
-    }</h2><h2>${"Country:" + country}</h2>
+    var text = `<div class="temp"><h1>${temp + "°C"} </h1>
+    <p class="summary">${condi}</p></div>
+   <h2>${name}</h2><h2>${"Region:" + state}</h2><h2>${"Country:" + country}</h2>
     <p>${"uv index:" + uv}</p>`;
     $("#weatherInfo").html(text);
   }
