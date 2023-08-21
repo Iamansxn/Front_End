91df88779b7e10eca385b53f8394b68ac33ed727
Lol9x <aman99saxena@gmail.com>
2023-08-21 17:47:47 +0530

    updated

--- WeatherApp/weather.js
+++ WeatherApp/weather.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
   const baseUrl = "http://api.weatherapi.com/v1/";
   const apiKey = "ac513c2ccca6427c912105247231808";
+
   $("#cityname").on("keypress", function () {
     const city = $("#cityname").val();
+    $(".weatherInfo").css("display", "block");
     console.log(city);
     const url = `${baseUrl}current.json?key=${apiKey}&q=${city}`;
     console.log(url);
@@ -14,11 +16,11 @@ $(document).ready(function () {
         displayWeather(data);
       },
       error: function () {
-        $("#weatherInfo").html("<p>Error featching weather data.</p>");
+        $(".weatherInfo").html("<p>Error featching weather data.</p>");
       },
     });
   });
-
+  // Display Weather
   function displayWeather(data) {
     const name = data.location.name;
     const state = data.location.region;
@@ -27,9 +29,47 @@ $(document).ready(function () {
     const condi = data.current.condition.text;
     const uv = data.current.uv;
     var text = `<div class="temp"><h1>${temp + "°C"} </h1>
-    <p class="summary">${condi}</p></div>
-   <h2>${name}</h2><h2>${"Region:" + state}</h2><h2>${"Country:" + country}</h2>
-    <p>${"uv index:" + uv}</p>`;
-    $("#weatherInfo").html(text);
+   <div class = "summary"> <p id="text">${condi}</p>
+    <p id="uv">${"uv index:" + uv}</p>
+    </div></div>
+    <div class= "location"><h1 id="city">${name}</h1><h2 id="state">${state}</h2><h2 id="country">${country}</h2></div>
+    `;
+    // Changing styles according to weather condition
+    if (condi == "Rain") {
+      $(".weatherInfo").css(
+        "background-image",
+        "url('/WeatherApp/images/rain.jpg')"
+      );
+      $(".temp").css("color", "Silver");
+    } else if (condi == "Sunny") {
+      $(".weatherInfo").css(
+        "background-image",
+        "url('/WeatherApp/images/Sunny.jpg')"
+      );
+      $(".temp").css("color", "blue");
+    } else if (condi == "Mist") {
+      $(".weatherInfo").css(
+        "background-image",
+        "url('/WeatherApp/images/Mist.jpg')"
+      );
+      $("body").css("background", "rgb(142,158,173)");
+      $("body").css(
+        "background",
+        "linear-gradient(90deg, rgba(142,158,173,1) 34%, rgba(0,87,54,1) 75%)"
+      );
+      $(".temp").css("color", "white");
+    } else if (condi == "Clear") {
+      $(".weatherInfo").css(
+        "background-image",
+        "url('/WeatherApp/images/Clear.jpg')"
+      );
+      $(".temp").css("color", "white");
+      $("body").css("background", " rgb(101, 162, 218)");
+      $("body").css(
+        "background",
+        "linear-gradient(90deg,rgba(101, 162, 218, 1) 23%,rgba(198, 204, 223, 1) 81)"
+      );
+    }
+    $(".weatherInfo").html(text);
   }
 });
